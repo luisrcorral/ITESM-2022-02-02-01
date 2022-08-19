63b077f84d07ecb08aa0ee241f297ff6e3acbc9b
luisrcorral <luisrcorral@gmail.com>
2022-08-18 19:39:30 -0500

    Initial commit

---
+++ app/src/main/java/mx/tec/itesm_2022_02_02_01/MainActivity.kt
@@ -0,0 +1,43 @@
+package mx.tec.itesm_2022_02_02_01
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+
+// My single line comment
+
+/* My multiple
+line comment */
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val myButton : Button = findViewById<Button>(R.id.button)
+        val myEditText : EditText = findViewById<EditText>(R.id.EditText1)
+        val myTextView : TextView = findViewById<TextView>(R.id.textView)
+
+        var myString : String
+
+        myButton.setOnClickListener {
+            myString = myEditText.text.toString()
+
+            if (myString == "") {
+
+
+            } else {
+                   
+            }
+
+        }
+
+
+
+
+
+
+    }
+}
